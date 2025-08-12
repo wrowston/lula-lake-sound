0d89bb354a3302b734c88d632c761ad71f2a079d
Will Rowston <willrowston@gmail.com>
2025-08-12 10:21:16 -0600

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Lula Lake Sound | Recording Studio | Chattanooga, TN",
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
