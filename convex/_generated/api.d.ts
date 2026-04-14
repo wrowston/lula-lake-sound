e17cba6112d71a43b751cc7128abb3598613d267
Will Rowston <willrowston@gmail.com>
2026-04-14 08:25:18 -0600

    convex gen files

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,6 +10,8 @@
 
 import type * as admin_siteSettings from "../admin/siteSettings.js";
 import type * as cms from "../cms.js";
+import type * as cmsShared from "../cmsShared.js";
+import type * as cmsTypes from "../cmsTypes.js";
 import type * as inquiries from "../inquiries.js";
 import type * as migrations from "../migrations.js";
 import type * as seed from "../seed.js";
@@ -24,6 +26,8 @@ import type {
 declare const fullApi: ApiFromModules<{
   "admin/siteSettings": typeof admin_siteSettings;
   cms: typeof cms;
+  cmsShared: typeof cmsShared;
+  cmsTypes: typeof cmsTypes;
   inquiries: typeof inquiries;
   migrations: typeof migrations;
   seed: typeof seed;
